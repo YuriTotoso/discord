eeab6db94601666749f147fcf27024028ceb6f6f
YuriTotoso <70677666+YuriTotoso@users.noreply.github.com>
2021-10-14 00:24:09 -0300

    Add files via upload

--- src/YuriTotoso/discordv3/Main.php
+++ src/YuriTotoso/discordv3/Main.php
@@ -1,11 +1,22 @@
 <?php
 
-declare(strict_types=1);
-
 namespace YuriTotoso\discordv3;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends PluginBase{
+  
+  public function onEnable(){
+    $this->getLogger()->notice("---Plugin Acaba De Ser ligado - Criado Por YurihudsonXD---");
+}
 
+   public function onCommand(CommandSender $sender, Command $cmd,  string $label, array $args) : bool{
+        switch($cmd->getName()){
+            case "dc":
+              $sender->sendMessage("§7Discord Do SEUSERVIDOR:\n §ehttps://discord.gg/ \nEntre Já!");
+                               return true;
+        }
+   }
 }
