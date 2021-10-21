d0048a81cdd1c17ad426f1d48fbd4e978ea8a6d2
YuriTotoso <70677666+YuriTotoso@users.noreply.github.com>
2021-10-21 00:58:31 -0300

    Update Main.php

--- src/YuriTotoso/discordv3/Main.php
+++ src/YuriTotoso/discordv3/Main.php
@@ -9,13 +9,13 @@ use pocketmine\command\CommandSender;
 class Main extends PluginBase{
   
   public function onEnable(){
-    $this->getLogger()->notice("---Plugin Acaba De Ser ligado - Criado Por YurihudsonXD---");
+    $this->getLogger()->notice("---Plugin Has Just Been Turned On - Created By YurihudsonXD---");
 }
 
    public function onCommand(CommandSender $sender, Command $cmd,  string $label, array $args) : bool{
         switch($cmd->getName()){
             case "dc":
-              $sender->sendMessage("§7Discord Do SEUSERVIDOR:\n §ehttps://discord.gg/ \nEntre Já!");
+              $sender->sendMessage("§7Discord FROM YOUR SERVER:\n §ehttps://discord.gg/ \nEnter Now!");
                                return true;
         }
    }
